f1f316cd4017726d6d386eef1acb2af2ef3341e2
C.V. Karthik <57444657+cvkarthik@users.noreply.github.com>
2021-02-22 11:44:15 +0530

    Add files via upload

---
+++ assignment6/sotedarraytobst.java
@@ -0,0 +1,30 @@
+class SortedArraytoBST{
+    static Node root;
+    Node sortedArrayToBST(int arr[], int start, int end) { 
+        if (start > end) { 
+            return null; 
+        } 
+        int mid = (start + end) / 2; 
+        Node node = new Node(arr[mid]); 
+        node.left = sortedArrayToBST(arr, start, mid - 1); 
+        node.right = sortedArrayToBST(arr, mid + 1, end); 
+          
+        return node; 
+    } 
+    void preOrder(Node node) { 
+        if (node == null) { 
+            return; 
+        } 
+        System.out.print(node.key + " "); 
+        preOrder(node.left); 
+        preOrder(node.right); 
+    } 
+    public static void main(String[] args) {
+        SortedArraytoBST tree=new SortedArraytoBST(); 
+        int[] arr ={1, 2, 3, 4, 5, 6, 7}; 
+        int n=arr.length; 
+        root=tree.sortedArrayToBST(arr, 0, n - 1); 
+        System.out.println("preorder traversal of BST is"); 
+        tree.preOrder(root); 
+    }
+}
